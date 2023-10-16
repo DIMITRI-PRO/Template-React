cf46ddfaa9f4e1e04e339b427fd977280491f476
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-10-16 21:40:01 +0200

    🧪 update add test

--- __tests__/testapp.test.js
+++ __tests__/testapp.test.js
@@ -1,5 +1,10 @@
+import { add } from "../src/function-test";
+
 describe("Test example", () => {
-  test("Should return true", () => {
-    expect(true).toBeTruthy();
+  test("Should return sum", () => {
+    const a = 1;
+    const b = 2;
+
+    expect(add(a, b)).toBe(3);
   });
 });
