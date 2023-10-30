c86d2d89ed639b6804338062bacda8926a2d7d0a
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-10-30 11:05:48 +0100

    🐛 fix test passed setup

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
 export default {
-  transform: {
-    "^.+\\.js$": "babel-jest",
-    "^.+\\.jsx?$": "babel-jest"
-  },
   testMatch: ["**/__tests__/**/*.test.(js|jsx|ts|tsx)"],
   testPathIgnorePatterns: ["/node_modules/", "/src/assets/"],
   coveragePathIgnorePatterns: ["/node_modules/", "/src/assets/"],
-  moduleNameMapper: {
-    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
-  },
   collectCoverage: true,
   coverageReporters: ["text", "cobertura"]
 };
