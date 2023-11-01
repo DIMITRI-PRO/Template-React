74da01a8147f428741dd6104087e478b2488c6b7
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-11-01 13:54:04 +0100

    🔧 update : base vite config deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "template-react",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
